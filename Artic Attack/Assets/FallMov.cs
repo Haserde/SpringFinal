a3e53f8e7224b6ff6f8faf8ec3ced90aaef8cdba
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-05-04 18:12:54 -0400

    TriggerField Made

---
+++ Artic Attack/Assets/FallMov.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallMov : MonoBehaviour {
+
+	public bool activ = false;
+	public float fallspd;
+	Rigidbody2D rb;
+
+	void Start () {
+	
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+
+	void Update () {
+	
+		if (activ) {
+			
+			rb.isKinematic = false;
+			rb.AddForce (new Vector2 (0, fallspd));
+
+		}
+
+	}
+
+	public void Activ (bool act) {
+
+		activ = act;
+
+	}
+
+}
---
+++ Artic Attack/Assets/TriggerField.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerField : MonoBehaviour {
+
+	public GameObject[] Wires;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+
+		if (col.gameObject.CompareTag ("Player")) {
+			foreach (GameObject wire in Wires) {
+				wire.SendMessage ("Activ", true);
+			}
+
+			Destroy (gameObject);
+
+		} 
+
+	}
+}

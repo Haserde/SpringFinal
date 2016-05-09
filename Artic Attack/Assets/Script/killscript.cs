0e52fc5f875a7f527940b2dd814d205d2486828d
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-05-09 17:40:07 -0400

    Connecting the PowerPlant scenes

---
+++ Artic Attack/Assets/Script/killscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killscript : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+
+		if (col.gameObject.CompareTag ("Player")) {
+
+			Destroy (col.gameObject);
+
+		}
+
+	}
+}

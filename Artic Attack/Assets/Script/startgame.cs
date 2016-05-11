97cbd19bea76bae87d153765fc84eafc1e945c04
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-05-11 16:35:04 -0400

    Tested connections between scenes
    All scenes connected

---
+++ Artic Attack/Assets/Script/startgame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startgame : MonoBehaviour {
+
+	public string nextLevel;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+		if (Input.GetButtonDown("Jump")) {
+			Application.LoadLevel (nextLevel);
+		}
+
+
+	}
+}

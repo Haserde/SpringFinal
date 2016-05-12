314477d1af810d6ecf47b409de8eba2df20fb03a
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MacBook-Pro.local>
2016-05-12 15:27:00 -0400

    Final changes, made connect between power plant and end screen

---
+++ Artic Attack/Assets/activate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activate : MonoBehaviour {
+
+	public bool activ = false;
+	public string nextLevel;
+	float timer = 100f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+		if (activ) {
+
+			timer--;
+			gameObject.GetComponent<Animator> ().SetBool ("fail", true);
+
+		}
+
+		if (timer < 0) {
+
+			Application.LoadLevel (nextLevel);
+
+		}
+
+	}
+
+	public void Activ () {
+
+		activ = true;
+
+	}
+}

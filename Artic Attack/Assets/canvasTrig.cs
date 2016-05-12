772c100b42262969c072552124992833b29cd3f5
Hasibe Erdemir <erdeh815@newschool.edu>
2016-05-12 16:18:50 -0400

    added shitty text

---
+++ Artic Attack/Assets/canvasTrig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class canvasTrig : MonoBehaviour {
+
+	public GameObject can;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			can.SetActive (true);
+		} else {
+			can.SetActive (false);
+		}
+	
+	}
+}

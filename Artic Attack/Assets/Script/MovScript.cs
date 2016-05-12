f517a4738d536065a57a3c030faf2899499bca8f
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MacBook-Pro.local>
2016-05-12 07:28:02 -0400

    Finished Side RAycasts for PlayerCharacter Started to work on the Scene Spawner

--- Artic Attack/Assets/Script/MovScript.cs
+++ Artic Attack/Assets/Script/MovScript.cs
@@ -9,6 +9,8 @@ public class MovScript : MonoBehaviour {
 	float xspd;
 	public float jumpspd = 1000f;
 	bool canjump;
+	bool canl = true;
+	bool canr = true;
 	int direction;
 	Rigidbody2D rb;
 	Rigidbody2D colrb;
@@ -20,6 +22,7 @@ public class MovScript : MonoBehaviour {
 		canjump = false;
 
 	}
+		
 
 	void FixedUpdate() {
 
@@ -27,21 +30,55 @@ public class MovScript : MonoBehaviour {
 		RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(transform.position.x-.8f,transform.position.y), -Vector2.up,2.0f,layerMask);
 		RaycastHit2D hit3 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y), -Vector2.up,2.0f,layerMask);
 
-		print ("hit: " + (hit2.collider != null));
-
 		if (hit1.collider != null || hit2.collider != null || hit3.collider != null) {
 			canjump = true;
 		} else {
 			canjump = false;
 		} 
+			
+		RaycastHit2D hit4 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y+1f), Vector2.right,1.2f,layerMask);
+		RaycastHit2D hit5 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y), Vector2.right,1.2f,layerMask);
+		RaycastHit2D hit6 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y-1f), Vector2.right,1.2f,layerMask);
+
+		RaycastHit2D hit7 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y+1f), Vector2.left,1.2f,layerMask);
+		RaycastHit2D hit8 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y), Vector2.left,1.2f,layerMask);
+		RaycastHit2D hit9 = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y-1f), Vector2.left,1.2f,layerMask);
+
+		if (hit4.collider != null || hit5.collider != null || hit6.collider != null) {
+			canr = false;
+		} else {
+			canr = true;
+		}
+
+		if (hit7.collider != null || hit8.collider != null || hit9.collider != null) {
+			canl = false;
+		} else {
+			canl = true;
+		}
+
 
 	}
 
 	void Update () {
 
-		xspd = Input.GetAxis("Horizontal") * movspd;
+		if (Input.GetAxis ("Horizontal") > 0) {
+			if (canr == true) {
+				xspd = Input.GetAxis("Horizontal") * movspd;
+				rb.velocity = new Vector2 (xspd, rb.velocity.y);
+			} else {
+				xspd = 0;
+			}
+		}
 
-		rb.velocity = new Vector2 (xspd, rb.velocity.y);
+		if (Input.GetAxis ("Horizontal") < 0) {
+			if (canl == true) {
+				xspd = Input.GetAxis ("Horizontal") * movspd;
+				rb.velocity = new Vector2 (xspd, rb.velocity.y);
+			} else {
+				xspd = 0;
+			}
+		}
+
 
 		if (Input.GetButtonDown ("Jump") && canjump) {
 			rb.AddForce (new Vector2 (0, jumpspd));
---
+++ Artic Attack/Assets/spawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	static public bool dir = true;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}

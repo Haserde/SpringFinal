4e2110b5ca93ec72704400cdfcc7363c5321b78b
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MacBook-Pro.local>
2016-05-12 14:50:33 -0400

    Adjustments to falling Bridge in power plant 2

--- Artic Attack/Assets/Script/FallMov.cs
+++ Artic Attack/Assets/Script/FallMov.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FallMov : MonoBehaviour {
 
-	public bool activ = false;
+	bool activ = false;
 	public float fallspd;
 	Rigidbody2D rb;
 
@@ -24,9 +24,13 @@ public class FallMov : MonoBehaviour {
 
 	}
 
-	public void Activ (bool act) {
+	void OnCollisionEnter2D (Collision2D col) {
 
-		activ = act;
+		if (col.gameObject.CompareTag ("Player")) {
+
+			activ = true;
+
+		}
 
 	}
 
